4a39a51fd8280ba39b65633eafda6383a85c770d
Michael <michael.leonov96@mail.ru>
2023-01-14 21:19:21 +0300

    add connect with mongodb and loggerUrl for restapi requests

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import router from "./routes";
 import mongoose from "mongoose";
 import ErorrHandlingMiddleware from "./middlewares/ErorrHandlingMiddleware";
+import loggerURL from "./middlewares/loggerURL";
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,15 +12,14 @@ const app = express();
 
 app.use(cors());
 app.use(json());
+app.use(loggerURL);
 app.use("/api", router);
 
 app.use(ErorrHandlingMiddleware);
 
 const start = async () => {
   try {
-    await mongoose.connect(
-      `mongodb://${process.env.DB_HOST}:27017/${process.env.DB_NAME}`
-    );
+    await mongoose.connect(process.env.DB_URL);
     console.log("Connected to MongoDB");
     app.listen(PORT, () => console.log(`Server start on ${PORT}`));
   } catch (error) {
