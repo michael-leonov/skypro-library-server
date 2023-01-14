00db8786604ae23c5e13eace16e1c5d07c650b5f
Michael <michael.leonov96@mail.ru>
2023-01-14 21:19:45 +0300

    add models

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,6 @@
-const userSchema = new mongoose.Schema({
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
   name: {
     type: String,
     required: true,
@@ -15,3 +17,7 @@ const userSchema = new mongoose.Schema({
     minlength: 2,
   },
 });
+
+const User = model("users", userSchema);
+
+export default User;
